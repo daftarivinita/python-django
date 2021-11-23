e6b7c1f34bc7ddfd519324ed71417598a7e3d580
vinita-daftari <daftarivinita@gmail.com>
2021-11-22 23:17:47 -0800

    fix mistake

--- DjangoExtra/api_project/api_project/settings.py
+++ DjangoExtra/api_project/api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$l&a4ny4)nu-fo^0of=k4$q*3$qxwv%==_eurvm=^lkikz-0_o'
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
